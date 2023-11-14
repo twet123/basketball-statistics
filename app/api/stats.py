39ff79ac61b29ba052a6d31ff8e4573afa619f17
Vladimir Popov <vladapopov02@gmail.com>
2023-11-14 21:45:37 +0100

    feat: finish stats route implementation

--- app/api/stats.py
+++ app/api/stats.py
@@ -1,16 +1,28 @@
-from sqlalchemy.orm import Session
-from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session, Query
+from fastapi import APIRouter, Depends, HTTPException
 
-from .. import deps, models
+from .. import deps, models, schemas
 
 router = APIRouter()
 
 
-@router.get("/stats/player/{player_full_name}")
-def get_player_statistics(player_full_name: str):
-    return {"player_full_name": player_full_name}
+@router.get("/stats/player/{player_full_name}", response_model=schemas.PlayerStats)
+def get_player_statistics(player_full_name: str, db: Session = Depends(deps.get_db)):
+    query: Query[models.Performance] = db.query(models.Performance).filter(
+        models.Performance.player == player_full_name
+    )
+    count: int = query.count()
 
+    if count == 0:
+        raise HTTPException(404, "Player not found")
 
-@router.get("/stats")
-def get_all_peformances(db: Session = Depends(deps.get_db)):
-    return db.query(models.Performance).all()
+    response = schemas.PlayerStats()
+    response.playerName = player_full_name
+    response.gamesPlayed = count
+
+    for performance in query.yield_per(1000):
+        response.add_performance(performance)
+
+    response.calculate_averages()
+
+    return response
