8a6e37843267931fdb0484845c847f611c200aae
Vladimir Popov <vladapopov02@gmail.com>
2023-11-14 13:05:57 +0100

    feat: add static pooling so that in-memory db persists throughout the threads

--- app/database.py
+++ app/database.py
@@ -1,11 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import StaticPool
 
 from .config import settings
 
 Base = declarative_base()
 
-engine = create_engine(settings.sqlalchemy_url)
+engine = create_engine(
+    settings.sqlalchemy_url,
+    poolclass=StaticPool,
+    connect_args={"check_same_thread": False},
+)
 
 SessionLocal = sessionmaker(bind=engine)
