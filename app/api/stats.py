e92277877597e672199367e75d518584117f7f99
Vladimir Popov <vladapopov02@gmail.com>
2023-11-14 13:06:47 +0100

    feat: test route to get all db rows

--- app/api/stats.py
+++ app/api/stats.py
@@ -1,4 +1,7 @@
-from fastapi import APIRouter
+from sqlalchemy.orm import Session
+from fastapi import APIRouter, Depends
+
+from .. import deps, models
 
 router = APIRouter()
 
@@ -6,3 +9,8 @@ router = APIRouter()
 @router.get("/stats/player/{player_full_name}")
 def get_player_statistics(player_full_name: str):
     return {"player_full_name": player_full_name}
+
+
+@router.get("/stats")
+def get_all_peformances(db: Session = Depends(deps.get_db)):
+    return db.query(models.Performance).all()
