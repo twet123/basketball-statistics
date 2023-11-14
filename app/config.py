0c78ed98dbe6969aa84e24f06f6f2de341a202de
Vladimir Popov <vladapopov02@gmail.com>
2023-11-14 12:34:35 +0100

    feat: add path to the test file in config.py

--- app/config.py
+++ app/config.py
@@ -2,6 +2,7 @@ class Settings:
     project_name: str = "Basketball Statistics API"
     version: str = "1.0.0"
     sqlalchemy_url: str = "sqlite://"
+    csv_source_file: str = "res/L9HomeworkChallengePlayersInput.csv"
 
 
 settings = Settings()
