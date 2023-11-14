59009fae3d712d5b37b1e818cef53d99c9c37497
Vladimir Popov <vladapopov02@gmail.com>
2023-11-14 12:36:23 +0100

    feat: add lifespan function for database initialization

--- app/main.py
+++ app/main.py
@@ -1,7 +1,31 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
+import csv
+from sqlalchemy.orm import Session
+
 from .config import settings
 from .api import stats
+from .database import SessionLocal, engine, Base
+from .models import Performance
 
-app = FastAPI(title=settings.project_name, version=settings.version)
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    Base.metadata.create_all(bind=engine)
+    db: Session = SessionLocal()
+
+    with open(settings.csv_source_file, "r", encoding="utf-8-sig") as csv_file:
+        reader: csv.DictReader = csv.DictReader(csv_file)
+
+        row: dict[str, str]
+        for row in reader:
+            performance = Performance(row)
+            db.add(performance)
+
+    db.close()
+    yield
+
+
+app = FastAPI(title=settings.project_name, version=settings.version, lifespan=lifespan)
 
 app.include_router(stats.router, tags=["stats"])
